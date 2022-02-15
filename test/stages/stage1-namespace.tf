27e39605460048e0c3259f8b8a4e3dee03495f2d
Sasikanth G <bgumpana@in.ibm.com>
2022-02-15 16:34:57 +0530

    Watson Studio on CP4D
    Signed-off-by: Sasikanth G <bgumpana@in.ibm.com>

--- test/stages/stage1-namespace.tf
+++ test/stages/stage1-namespace.tf
@@ -30,6 +30,18 @@ module "gitops_cpd_operator_namespace" {
   create_operator_group = true
 }
 
+module "gitops_cpd_namespace" {
+  depends_on = [
+    module.gitops_cpd_operator_namespace
+  ]
+  source = "github.com/cloud-native-toolkit/terraform-gitops-namespace.git"
+
+  gitops_config = module.gitops.gitops_config
+  git_credentials = module.gitops.git_credentials
+  name = var.cpd_operator_namespace
+  create_operator_group = false
+}
+
 
 resource null_resource write_namespace {
   provisioner "local-exec" {
@@ -41,12 +53,33 @@ resource null_resource write_namespace {
   provisioner "local-exec" {
     command = "echo -n '${module.gitops_cpd_operator_namespace.name}' > .operator_namespace"
   }
+
+  provisioner "local-exec" {
+    command = "echo -n '${module.gitops_cpd_namespace.name}' > .cpd_namespace"
+  }
 }
 
 
 module cs_pull_secret {
   depends_on = [
-    module.gitops_cpd_operator_namespace
+    module.gitops_cpd_namespace
+  ]
+  source = "github.com/cloud-native-toolkit/terraform-gitops-pull-secret"
+
+  gitops_config = module.gitops.gitops_config
+  git_credentials = module.gitops.git_credentials
+  server_name = module.gitops.server_name
+  kubeseal_cert = module.gitops.sealed_secrets_cert
+  namespace = var.cpd_common_services_namespace
+  docker_username = "cp"
+  docker_password = var.cp_entitlement_key
+  docker_server   = "cp.icr.io"
+  secret_name     = "ibm-entitlement-key"
+}
+
+module cpd_op_pull_secret {
+  depends_on = [
+    module.cs_pull_secret
   ]
   source = "github.com/cloud-native-toolkit/terraform-gitops-pull-secret"
 
@@ -60,9 +93,10 @@ module cs_pull_secret {
   docker_server   = "cp.icr.io"
   secret_name     = "ibm-entitlement-key"
 }
+
 module cpd_pull_secret {
   depends_on = [
-    module.cs_pull_secret
+    module.cpd_op_pull_secret
   ]
   source = "github.com/cloud-native-toolkit/terraform-gitops-pull-secret"
 
